30610ee80cedd1060921f650209c87512745b69b
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-12 22:43:42 +0530

    Update hash_test1.cs

--- hash_test1.cs
+++ hash_test1.cs
@@ -4,16 +4,16 @@ namespace HashTables_BST_Day15
 {
     class Program
     {
-        public static void CountingWordFrequency(string sentence)            //this static method counts thr frquency of the words.
+        public static void CountingWordFrequency(string sentence)         
         {
-            MyMapNode<string, int> hashtable = new MyMapNode<string, int>(10);      //declaring the key and value data types of the Mymapnodde class.
-            string[] words = sentence.Split();                //splitting the sentence otherwise it wont be possible to generate keys.
+            MyMapNode<string, int> hashtable = new MyMapNode<string, int>(10);      
+            string[] words = sentence.Split();                
             foreach (string word in words)
             {
-                if (hashtable.Exists(word))                    //word is the "key"
+                if (hashtable.Exists(word))                    
                 {
-                    hashtable.Add(word, hashtable.Get(word) + 1);           //if it exists. get the value of the linkedlist object and invrement its value.
-                }
+                    hashtable.Add(word, hashtable.Get(word) + 1);           
+                    }
                 else
                 {
                     hashtable.Add(word, 1);
@@ -21,7 +21,7 @@ namespace HashTables_BST_Day15
             }
             Console.WriteLine("Displaying after add operation\n");
             hashtable.Display();
-            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("");
         }
         static void Main(string[] args)
         {
